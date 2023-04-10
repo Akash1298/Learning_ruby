60cc2246027e7612ff0eb3868b638b5557e03416
Akash1298 <akashrana0153@gmail.com>
2023-04-10 09:39:26 +0530

    notes

---
+++ notes.rb
@@ -0,0 +1,10 @@
+# class names: CamelCase
+#     class ThisIsCamelCase
+#       # code for class
+#     end
+
+
+
+# file names, method names and variable names: snake_case
+# Variable assignment:
+# this_is_snake_case = "snake case"
